6e246167bb2f5e4ca0863a19a22146927c4cfd22
joao lucas <jlmelosouza18@gmail.com>
2025-02-20 21:34:50 -0300

    add backend url in .env

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: import.meta.env.BACKEND_URL,
 })
 
 export const axiosPrivate = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: import.meta.env.BACKEND_URL,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 })
